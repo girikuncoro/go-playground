76c5d2d0789d780a74c7fe5e2e85cd0474e3a613
Giri Kuncoro <girikuncoro@gmail.com>
2018-02-20 17:45:03 -0800

    Refactor list command to new cli builder

--- bookshelf/cmd/list.go
+++ bookshelf/cmd/list.go
@@ -4,12 +4,24 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var listCmd = &cobra.Command{
-	Use:   "list",
-	Short: "a namespace for listing books",
-	Long:  `From here you can list books`,
-}
+type listCmd struct{}
 
-func init() {
-	RootCmd.AddCommand(listCmd)
+func registerListCmds(cli *Cli) {
+	cli.listCmd = &listCmd{}
+
+	listCmd := &cobra.Command{
+		Use:   "list",
+		Short: "a namespace for listing books",
+		Long:  `From here you can list books`,
+	}
+
+	listBookCmd := &cobra.Command{
+		Use:   "book",
+		Short: "list books",
+		Long:  `List books from various title and author`,
+		RunE:  cli.runner(listBookRun),
+	}
+
+	listCmd.AddCommand(listBookCmd)
+	cli.rootCmd.AddCommand(listCmd)
 }
--- bookshelf/cmd/list_book.go
+++ bookshelf/cmd/list_book.go
@@ -2,33 +2,23 @@ package cmd
 
 import (
 	"fmt"
-	"log"
-
-	bk "github.com/girikuncoro/go-playground/bookshelf/pkg"
-	"github.com/spf13/cobra"
 )
 
-var listBookCmd = &cobra.Command{
-	Use:   "book",
-	Short: "list books",
-	Long:  `List books from various title and author`,
-	Run: func(cmd *cobra.Command, args []string) {
-		db, err := bk.NewMySQLDB(bk.MySQLConfig{
-			Username: "root",
-			Password: "my-secret-pw",
-			Host:     "0.0.0.0",
-			Port:     9000,
-		})
-		if err != nil {
-			log.Fatalln(err)
-		}
-		books, err := db.ListBooks()
-		for _, book := range books {
-			fmt.Printf("%#v", book)
-		}
-	},
-}
+func listBookRun(cli *Cli) error {
+	db, err := cli.dbClient(cli.dbUser, cli.dbPassword, cli.dbHost, cli.dbPort)
+	if err != nil {
+		fmt.Printf("%v", err)
+		return err
+	}
 
-func init() {
-	listCmd.AddCommand(listBookCmd)
+	books, err := db.ListBooks()
+	if err != nil {
+		fmt.Printf("%v", err)
+		return err
+	}
+
+	for _, book := range books {
+		fmt.Printf("%v\n", book)
+	}
+	return nil
 }
