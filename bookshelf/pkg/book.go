574b5e0d80e842a685dc1c2656b8246bac6cc64c
Giri Kuncoro <girikuncoro@gmail.com>
2018-02-20 16:22:19 -0800

    Simplify book struct

--- bookshelf/pkg/book.go
+++ bookshelf/pkg/book.go
@@ -1,32 +1,29 @@
 package bookshelf
 
+import "fmt"
+
 type Book struct {
 	ID            int64
 	Title         string
 	Author        string
 	PublishedDate string
-	CreatedBy     string
-	CreatedByID   string
-}
-
-func (b *Book) CreatedByDisplayName() string {
-	if b.CreatedBy == "anonymous" {
-		return "Anonymous"
-	}
-	return b.CreatedBy
-}
-
-func (b *Book) SetCreatorAnonymous() {
-	b.CreatedBy = ""
-	b.CreatedBy = "anonymous"
 }
 
 type BookDatabase interface {
 	ListBooks() ([]*Book, error)
-	ListBooksCreatedBy(userID string) ([]*Book, error)
-	GetBook(id int64) (*Book, error)
 	AddBook(b *Book) (id int64, err error)
-	DeleteBook(id int64) error
-	UpdateBook(b *Book) error
 	Close()
+}
+
+func (b *Book) Validate() error {
+	if b.Title == "" {
+		return fmt.Errorf("Title must be provided")
+	}
+	if b.Author == "" {
+		return fmt.Errorf("Author must be provided")
+	}
+	if b.PublishedDate == "" {
+		return fmt.Errorf("Published date must be provided")
+	}
+	return nil
 }
